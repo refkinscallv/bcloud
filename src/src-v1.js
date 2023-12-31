26e222833a0af2a4c30ec9329ff839a98ab4f515
Refkinscallv <refkinscallv@gmail.com>
2023-12-31 21:43:16 +0700

    update #943

--- src/src-v1.js
+++ src/src-v1.js
@@ -5,11 +5,12 @@ document.addEventListener("DOMContentLoaded", () => {
     if(!window.jQuery){
         console.log("BCloud : Error Requirements\n\njQuery is not installed. Please install first with a minimum version of 3.6");
 
-        var script = document.createElement("script");
-        script.src = "https://code.jquery.com/jquery-3.7.1.min.js";
-        script.integrity = "sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=";
-        script.crossOrigin = "anonymous";
-        document.head.prependChild(script);
+        const jQuerySRC         = document.createElement("script");
+        jQuerySRC.src           = "https://code.jquery.com/jquery-3.7.1.min.js";
+        jQuerySRC.integrity     = "sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=";
+        jQuerySRC.crossOrigin   = "anonymous";
+        var bCloudSRC           = document.querySelector('script[data-bcloud]');
+        bCloudSRC.prepend(jQuerySRC);
     }
 
     bcloudRenderElement();
