5d739b22c297e4f4f66c2a95b2ed2394672896ab
Refkinscallv <refkinscallv@gmail.com>
2023-12-31 18:36:45 +0700

    update src-v1.js call

--- application/controllers/Src.php
+++ application/controllers/Src.php
@@ -30,7 +30,7 @@
                     if($check){
                         $status = true;
                         $source = "const baseUrl = '". base_url() ."'";
-                        $source .= file_get_contents("src/src-v1.js");
+                        $source .= file_get_contents("/src/src-v1.js");
                     } else {
                         http_response_code(403);
                         $status = false;
