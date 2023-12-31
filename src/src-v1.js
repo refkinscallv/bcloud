4d6c3b0c86b4844b68b57c6d3e55844808bd7492
Refkinscallv <refkinscallv@gmail.com>
2023-12-31 21:49:56 +0700

    update #949

--- src/src-v1.js
+++ src/src-v1.js
@@ -11,9 +11,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
 $(document).ready(() => {
     bcloudRenderElement();
+    bCloudSetup();
 });
 
-function setup(){
+function bcloudRenderElement(){
+    $("body").append(`<div class="bcloud-panel"></div>`);
+    $(".bcloud-panel").hide();
+}
+
+function bCloudSetup(){
     const bCloudQuery       = new URLSearchParams(window.location.search);
     const bCloudQueryPanel  = bCloudQuery.get('bcloud');
 
@@ -21,9 +27,4 @@ function setup(){
         $(".bcloud-on-panel").hide();
         $(".bcloud-panel").show();
     }
-}
-
-function bcloudRenderElement(){
-    $("body").append(`<div class="bcloud-panel"></div>`);
-    $(".bcloud-panel").hide();
 }
