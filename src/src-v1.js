ca12eba31bfe9f44b7a07fb71f8142965b2bbe60
Refkinscallv <refkinscallv@gmail.com>
2023-12-31 22:30:06 +0700

    update #1030

--- src/src-v1.js
+++ src/src-v1.js
@@ -9,15 +9,21 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 });
 
+/**
+ * Defer & Declared
+ */
 $(document).ready(() => {
     const bCloudQuery       = new URLSearchParams(window.location.search);
-    const bCloudQueryParam  = bCloudQuery.get('bcloud');
+    const bCloudQueryParam  = bCloudQuery.get("bcloud");
 
     bCloud.renderElement();
     bCloud.setup(bCloudQueryParam);
     bCloud.route(bCloudQuery);
 });
 
+/**
+ * Init
+ */
 const bCloud = {
     renderElement : () => {
         $("body").append(`<div class="bcloud-panel"></div>`);
@@ -29,9 +35,46 @@ const bCloud = {
             $(".bcloud-on-panel").hide();
             $(".bcloud-panel").show();
         }
+
+        /**
+         * Set Other Requirements
+         */
+        $("head").append(`<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">`);
+        $("head").append(`<link href="https://cdn.jsdelivr.net/npm/remixicon@4.0.0/fonts/remixicon.css" rel="stylesheet"/>`);
+        $("head").append(`<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>`);
+        $("body").append(`<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>`);
     },
 
     route : (path) => {
-        console.log(path.get('loc'));
+        switch(path.get("loc")){
+            case "home" : 
+                bCloudController.home();
+                break;
+            case "login" : 
+                bCloudController.login();
+                break;
+            default : 
+                location.href = baseUrl;
+                break;
+        }
     }
 }
+
+/**
+ * Controller
+ */
+const bCloudController = {
+    home : () => {
+
+    }
+}
+
+/**
+ * Request
+ */
+const bCloudRequest = {}
+
+/**
+ * Helper
+ */
+const bCloudHelper = {}
