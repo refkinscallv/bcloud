425bd7fe69345dcad7c3307522fda1789b50e4c0
Refkinscallv <refkinscallv@gmail.com>
2023-12-31 18:20:53 +0700

    update again

--- application/controllers/Src.php
+++ application/controllers/Src.php
@@ -21,31 +21,32 @@
         public function v1($token = false){
             if(isset($_SERVER["HTTP_REFERER"])){
                 if($token){
-                    $referer    = parse_url($_SERVER["HTTP_REFERER"], PHP_URL_HOST);
-                    $check      = $this->src->check_users($referer, $token);
+                    $referer = parse_url($_SERVER["HTTP_REFERER"], PHP_URL_HOST);
+                    $check = $this->src->check_users($referer, $token);
 
                     if($check){
-                        $status     = true;
+                        $status = true;
                         $source = "const baseUrl = '". base_url() ."'";
                         $source .= file_get_contents(base_url("src/src-v1.js"));
                     } else {
                         http_response_code(403);
-                        $status     = false;
+                        $status = false;
                         $source = "alert(Origin of request is unregistered)";
                     }
                 } else {
                     http_response_code(403);
-                    $status     = false;
+                    $status = false;
                     $source = "alert(Forbidden Access)";
                 }
             } else {
                 http_response_code(403);
-                $status     = false;
+                $status = false;
                 $source = "alert(Origin of request is unknown)";
             }
 
-            header("Content-Type: Text/JavaScript");
+            header("Content-Type: text/javascript");
             echo $source;
+            exit; // Exit to prevent further code execution
         }
 
     }
