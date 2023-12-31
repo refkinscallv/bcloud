b60939d41f27cab0072ad1b6144b11961a19de44
Refkinscallv <refkinscallv@gmail.com>
2023-12-31 21:22:08 +0700

    update 922

--- src/src-v1.js
+++ src/src-v1.js
@@ -1,8 +1,14 @@
-alert("sample");
+document.addEventListener("DOMContentLoaded", () => {
+    if(!window.jQuery){
+        console.log("BCloud : Error Requirements\n\njQuery is not installed. Please install first with a minimum version of 3.6");
 
-window.onload = (event) => {
-    $("header").append('<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>');
-};
+        var script = document.createElement("script");
+        script.src = "https://code.jquery.com/jquery-3.7.1.min.js";
+        script.integrity = "sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=";
+        script.crossOrigin = "anonymous";
+        document.head.appendChild(script);
+    }
+});
 
 if(window.jQuery){
     alert("sample");
