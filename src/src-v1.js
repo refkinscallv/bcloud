0da2d4ea92e4ffe4847cd8aeb3bb8d7f0155f93f
Refkinscallv <refkinscallv@gmail.com>
2024-01-01 00:29:33 +0700

    update #1120231229
    update JS Cookies

--- src/src-v1.js
+++ src/src-v1.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
     } else {
         $("head").append(`<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">`);
         $("head").append(`<link href="https://cdn.jsdelivr.net/npm/remixicon@4.0.0/fonts/remixicon.css" rel="stylesheet"/>`);
-        $("head").append(`<script src="https://cdn.jsdelivr.net/gh/carhartl/jquery-cookie@releases/tag/v1.4.1"></script>`);
+        $("head").append(`<script src=" https://cdn.jsdelivr.net/npm/js-cookie@3.0.5/dist/js.cookie.min.js "></script>`);
         $("head").append(`<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>`);
         $("body").append(`<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>`);
     }
