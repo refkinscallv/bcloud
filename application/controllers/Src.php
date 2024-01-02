a607aa3697c3f6d7a14e68ceff329d7444736787
Refkinscallv <refkinscallv@gmail.com>
2024-01-03 01:34:59 +0700

    versioning

--- application/controllers/Src.php
+++ application/controllers/Src.php
@@ -6,7 +6,6 @@
             parent::__construct();
 
             header("Access-Control-Allow-Origin: *");
-            header("Content-Type: text/javascript");
 
             $this->load->model([
                 "Src_model" => "src"
@@ -20,7 +19,7 @@
         /**
          * Versioning
          */
-        public function v1($token = false){
+        public function js($version = "1", $token = false){
 
             if(isset($_SERVER["HTTP_REFERER"])){
                 if($token){
@@ -30,7 +29,9 @@
                     if($check){
                         $status = true;
                         $source = "const bcURL  = '". base_url() ."';";
-                        $source .= file_get_contents("/home/cgaa9149/bcloud/src/src-v1.js");
+                        $source .= file_get_contents("/home/cgaa9149/bcloud/src/v". $version ."/bcloud.css");
+                        $source .= file_get_contents("/home/cgaa9149/bcloud/src/v". $version ."/bcloud.js");
+                        $source .= file_get_contents("/home/cgaa9149/bcloud/src/v". $version ."/bcloud-front.js");
                     } else {
                         http_response_code(403);
                         $status = false;
@@ -46,7 +47,40 @@
                 $status = false;
                 $source = "alert('Origin of request is unknown');";
             }
+            
+            header("Content-Type: text/javascript");
 
+            echo $source;
+        }
+
+        public function css($version = "1", $token = false){
+
+            if(isset($_SERVER["HTTP_REFERER"])){
+                if($token){
+                    $referer = parse_url($_SERVER["HTTP_REFERER"], PHP_URL_HOST);
+                    $check = $this->src->check_users($referer, $token);
+
+                    if($check){
+                        $status = true;
+                        $source .= file_get_contents("/home/cgaa9149/bcloud/src/v". $version ."/bcloud.css");
+                    } else {
+                        http_response_code(403);
+                        $status = false;
+                        $source = "alert('Origin of request is unregistered');";
+                    }
+                } else {
+                    http_response_code(403);
+                    $status = false;
+                    $source = "alert('Forbidden Access');";
+                }
+            } else {
+                http_response_code(403);
+                $status = false;
+                $source = "alert('Origin of request is unknown');";
+            }
+            
+            header("Content-Type: text/css");
+            
             echo $source;
         }
 
---
+++ src/v1/bcloud-front.js
@@ -0,0 +1,5 @@
+const bcFront   = {
+    login : `
+        <div id="bcLogin" class="position-relative vh-100 overflow-auto"></div>
+    `,
+}
